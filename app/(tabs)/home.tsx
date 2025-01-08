1c2d3921383288bb0c2aa1fddd66a06a0a9be325
daryloffik <quenumdaryl28@gmail.com>
2025-01-08 14:18:08 +0100

    feat:Finally found a way to get the songs using expo

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,63 @@
 import { Text, View, StyleSheet, Image } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+import { useState } from 'react';
+
+/*async function requestStoragePermission() {
+  const { status } = await MediaLibrary.requestPermissionsAsync(); // Expo 41+
+  if (status === 'granted') {
+    console.log('Permission de stockage accordée !');
+  } else {
+    console.log('Permission de stockage refusée.');
+  }
+} 
+
+requestStoragePermission();
+*/
+
 
 export default function Home()
 {
+    const [songs, setSongs] = useState<object[]>([]);
+
+
+async function requestPermission() {
+  const { status } = await MediaLibrary.requestPermissionsAsync();
+  if (status !== 'granted') {
+    console.log('Permission refusée');
+    return;
+  }
+  console.log('Permission accordée');
+}
+
+async function getDownloads() {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission refusée');
+      return;
+    }
+  
+    // Obtenir les fichiers du dossier Téléchargements
+    const assets = await MediaLibrary.getAssetsAsync({
+      mediaType: MediaLibrary.MediaType.audio, // Pour inclure tous les types de fichiers
+    });
+  
+    console.log('Fichiers trouvés :', assets.assets[2]);
+    setSongs(assets.assets);
+
+  }
+  
+  getDownloads();
+  
+
+requestPermission();
+
+      
     return (
         <View>
-           <Text> Home Miaou!</Text>
+           <Text>
+           {songs.length > 0 ? `Home Miaou! ${songs[0].albumId}` : 'Chargement des chansons...'}
+           </Text>
         </View>
     )
 
